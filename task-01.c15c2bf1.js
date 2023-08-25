1678fcb3a157e0615570bba1c3c47be8f08b781a
Titiof <136720570+Titiof@users.noreply.github.com>
2023-08-25 15:57:26 +0000

    Deploying to gh-pages from @ Titiof/goit-js-hw-06@7d4c39e1638fd6f3b42e1c81474eaf4a64283a18 🚀

---
+++ task-01.c15c2bf1.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".item");console.dir(`Number of categories: ${e.length}`),e.forEach((e=>console.log(`Category: ${e.querySelector("h2").textContent}\n\nElements: ${e.querySelectorAll("li").length}`)));
+//# sourceMappingURL=task-01.c15c2bf1.js.map
---
+++ task-01.dcc50a51.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll(".item");console.dir("Number of categories: ".concat(e.length)),e.forEach((function(e){return console.log("Category: ".concat(e.querySelector("h2").textContent,"\n\nElements: ").concat(e.querySelectorAll("li").length))}))}();
+//# sourceMappingURL=task-01.dcc50a51.js.map
